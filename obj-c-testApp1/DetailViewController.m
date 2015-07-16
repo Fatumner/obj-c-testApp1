fb75cd9320b83287e494fe2dc7dd0ce8569c3167
Mateusz Florczak <mateuszf@AIRELEMENTAL-2.local>
2015-07-16 12:43:56 +0200

    Added spinner for loading page

--- obj-c-testApp1/DetailViewController.m
+++ obj-c-testApp1/DetailViewController.m
@@ -36,4 +36,16 @@
 }
 */
 
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
 @end
