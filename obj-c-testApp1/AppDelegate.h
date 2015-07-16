27a378ad3d5bc015fecaa6873692dbea121c2c8f
Mateusz Florczak <mateuszf@AIRELEMENTAL-2.local>
2015-07-16 11:43:10 +0200

    added empty project

---
+++ obj-c-testApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  obj-c-testApp1
+//
+//  Created by Mateusz Florczak on 16/07/15.
+//  Copyright (c) 2015 Kainos Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
